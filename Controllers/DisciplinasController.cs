feaa8929fae04e5d2e5852be4fcae00a4c75ee13
Luiz Henrique Campos Rocha <luizhenriquecr1359@gmail.com>
2024-09-25 18:04:21 -0300

    Implementação da criptografia.

--- Controllers/DisciplinasController.cs
+++ Controllers/DisciplinasController.cs
@@ -1,9 +1,11 @@
 ﻿using mf_api_gerenciamento_tarefas_G14.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace mf_api_gerenciamento_tarefas_G14.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DisciplinasController : ControllerBase
