04d076ea6965a8b3a0f857d7f612d6a3d8e08583
Luiz Henrique Campos Rocha <luizhenriquecr1359@gmail.com>
2024-10-09 20:34:56 -0300

    Atualização de controllers e models.

--- Models/Disciplina.cs
+++ Models/Disciplina.cs
@@ -11,51 +11,16 @@ namespace mf_api_gerenciamento_tarefas_G14.Models
     public class Disciplina : BaseEntity
     {
         [Required(ErrorMessage = "É necessário selecionar uma disciplina.")]
-        public DisciplinasEnum Nome { get; set; }
+        public string Nome { get; set; }
 
         [Required(ErrorMessage = "É necessário selecionar o usuário.")]
         public int UsuarioId { get; set; }
 
         public Usuario Usuario { get; set; }
-       
+
         public ICollection<Nota> Notas { get; set; }
 
-        public Disciplina()
-        {
-            Notas = new HashSet<Nota>();
-        }
 
-
-        public static DisciplinasEnum ParseDisciplina(string disciplinaName)
-        {
-            disciplinaName = RemoveAccents(disciplinaName).ToLowerInvariant();
-            foreach (DisciplinasEnum disciplina in Enum.GetValues(typeof(DisciplinasEnum)))
-            {
-                if (RemoveAccents(disciplina.ToString()).ToLowerInvariant() == disciplinaName)
-                {
-                    return disciplina;
-                }
-            }
-            throw new ArgumentException("Disciplina inválida.");
-        }
-
-
-        private static string RemoveAccents(string text)
-        {
-            return string.Concat(text.Normalize(NormalizationForm.FormD)
-                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark))
-                .Normalize(NormalizationForm.FormC);
-        }
-    }
-
-    public enum DisciplinasEnum
-    {
-        [Display(Name = "Português")]
-        Portugues,
-        [Display(Name = "Matemática")]
-        Matematica,
-        [Display(Name = "Geografia")]
-        Geografia
     }
 }
 //<<<<<<< EndpointsDisciplina
