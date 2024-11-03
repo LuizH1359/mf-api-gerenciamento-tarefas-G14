4e00bcaed635df9e01b7f84e099985425fbe4a07
Luiz Henrique Campos Rocha <luizhenriquecr1359@gmail.com>
2024-11-03 12:14:12 -0300

    Atualicação de rotas.

--- Controllers/TarefasController.cs
+++ Controllers/TarefasController.cs
@@ -1,10 +1,12 @@
 ﻿using mf_api_gerenciamento_tarefas_G14.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace mf_api_gerenciamento_tarefas_G14.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TarefasController : ControllerBase
